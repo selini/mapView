d1601c7a9ae7d27dd276e06743af661b295ae478
Selini Kyriazidou <ikaros@ikaross-iMac.lan>
2015-08-13 09:39:40 +0300

    Initial Commit

---
+++ mapExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mapExample
+//
+//  Created by ikaros on 8/13/15.
+//  Copyright (c) 2015 ikaros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mapExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mapExample
+//
+//  Created by ikaros on 8/13/15.
+//  Copyright (c) 2015 ikaros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
